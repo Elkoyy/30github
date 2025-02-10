6fa155ebbab5f36a6e105a353f28a0c72baa948a
Elnura Ten <elkatyna.ru@gmail.com>
2025-02-10 18:23:04 +0600

    Create is_prime.py

---
+++ is_prime.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+num = int(input("Введите число: "))
+
+if is_prime(num):
+    print(f"{num} — простое число")
+else:
+    print(f"{num} — составное число")
